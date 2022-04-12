5daea857ea3e8019ffd11a8705d551af0de4c0ea
108062328 <sp21804173@gmail.com>
2022-04-12 15:27:30 +0800

    Initial commit

---
+++ public/js/config.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+/// TODO 1: Change to your firebase config
+///         1. New a Firebase project
+///         2. Copy and paste firebase config below
+///         Important: Make sure there is databaseURL in the config.
+///         If there is no databaseURL in the config, go to firebase console -> Realtime Database to find the databaseURL.
+var config = {
+
+};
+firebase.initializeApp(config);
---
+++ public/js/signin.js
@@ -0,0 +1,52 @@
+// Custom alert
+function create_alert(type, message) {
+    var alertarea = document.getElementById('custom-alert');
+    if (type == "success") {
+        str_html = "<div class='alert alert-success alert-dismissible fade show' role='alert'><strong>Success! </strong>" + message + "<button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button></div>";
+        alertarea.innerHTML = str_html;
+    } else if (type == "error") {
+        str_html = "<div class='alert alert-danger alert-dismissible fade show' role='alert'><strong>Error! </strong>" + message + "<button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button></div>";
+        alertarea.innerHTML = str_html;
+    }
+}
+
+function initApp() {
+    // Login with Email/Password
+    var txtEmail = document.getElementById('inputEmail');
+    var txtPassword = document.getElementById('inputPassword');
+    var btnLogin = document.getElementById('btnLogin');
+    var btnGoogle = document.getElementById('btngoogle');
+    var btnSignUp = document.getElementById('btnSignUp');
+
+    btnSignUp.addEventListener('click', function() {
+        /// TODO 2: Add email signup button event
+        ///         1. Get user input email and password to signup
+        ///         2. Show success message by "create_alert()" and clean input field
+        ///         3. Show error message by "create_alert()" and clean input field
+
+    });
+
+    btnLogin.addEventListener('click', function() {
+        /// TODO 3: Add email login button event
+        ///         1. Get user input email and password to login
+        ///         2. Back to index.html when login success
+        ///         3. Show error message by "create_alert()" and clean input field
+
+
+    });
+
+    btnGoogle.addEventListener('click', function() {
+        /// TODO 4: Add google login button event
+        ///         1. Use popup function to login google
+        ///         2. Back to index.html when login success
+        ///         3. Show error message by "create_alert()"
+
+
+    });
+
+
+}
+
+window.onload = function() {
+    initApp();
+};
